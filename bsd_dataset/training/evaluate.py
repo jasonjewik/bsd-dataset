34b182f38615f5f69f69885e570867352208c984
Shashank Goel <shashankgoel@mint1.cs.ucla.edu>
2022-06-01 10:44:13 -0700

    added metrics

--- bsd_dataset/training/evaluate.py
+++ bsd_dataset/training/evaluate.py
@@ -4,61 +4,36 @@ import logging
 import torch.nn as nn
 from tqdm import tqdm    
 from einops import rearrange
+from bsd_dataset.common.metrics import rmse, bias, pearson_correlation_coefficient
 
-def get_val_metrics(model, dataloader, options):
-    logging.info("Started validation")
-
+def get_metrics(model, dataloader, prefix, options):
     metrics = {}
 
     model.eval()
-    criterion = nn.MSELoss(reduction = "none").to(options.device)
 
-    losses = []
+    total_rmse = 0
+    total_bias = 0
+    total_pearson_correlation_coefficient = 0
 
     with torch.no_grad():
         for batch in tqdm(dataloader):
             context, target, mask = batch[0].to(options.device), batch[1].to(options.device), batch[2]["y_mask"].to(options.device)
-            target = target.nan_to_num()
             target = torch.log(target / 86400 + 0.1) - torch.log(torch.tensor(0.1))
             context[:, 4, :, :] = torch.log(context[:, 4, :, :] + 0.1) - torch.log(torch.tensor(0.1))
 
             predictions = model(context)
 
-            loss = (torch.square(predictions - target) * (1 - mask.float())).sum()
-            losses.append(loss)
+            total_rmse += rmse(predictions, target) * len(target)
+            total_bias += bias(predictions, target) * len(target)
+            total_pearson_correlation_coefficient += pearson_correlation_coefficient(predictions, target) * len(target)
 
-        loss = sum(losses) / dataloader.num_samples
-        metrics["val_loss"] = loss
+        total_rmse /= dataloader.num_samples
+        total_bias /= dataloader.num_samples
+        total_pearson_correlation_coefficient /= dataloader.num_samples
 
-    logging.info("Finished validation")
-
-    return metrics
-
-def get_test_metrics(model, dataloader, options):
-    logging.info("Started testing")
-
-    metrics = {}
-
-    model.eval()
-
-    losses = []
-
-    with torch.no_grad():
-        for batch in tqdm(dataloader):
-            context, target, mask = batch[0].to(options.device), batch[1].to(options.device), batch[2]["y_mask"].to(options.device)
-            target = target.nan_to_num()
-            target = torch.log(target / 86400 + 0.1) - torch.log(torch.tensor(0.1))
-            context[:, 4, :, :] = torch.log(context[:, 4, :, :] + 0.1) - torch.log(torch.tensor(0.1))
-
-            predictions = model(context)
-            
-            loss = (torch.square(predictions - target) * (1 - mask.float())).sum()
-            losses.append(loss)
-
-        loss = sum(losses) / dataloader.num_samples
-        metrics["test_loss"] = loss
-    
-    logging.info("Finished testing")
+        metrics[f"{prefix}_rmse"] = total_rmse
+        metrics[f"{prefix}_bias"] = total_bias
+        metrics[f"{prefix}_pearson"] = total_pearson_correlation_coefficient
 
     return metrics
 
@@ -67,19 +42,16 @@ def evaluate(epoch, model, dataloaders, options):
     
     if(options.master):
         if(dataloaders["val"] is not None or dataloaders["test"] is not None):
-            if(epoch == 0):
-                logging.info(f"Base evaluation")
-            else:
-                logging.info(f"Epoch {epoch} evaluation")
+            logging.info(f"Starting epoch {epoch} evaluation")
 
         if(dataloaders["val"] is not None): 
-            metrics.update(get_val_metrics(model, dataloaders["val"], options))
+            metrics.update(get_metrics(model, dataloaders["val"], "val", options))
             
         if(dataloaders["test"] is not None): 
-            metrics.update(get_test_metrics(model, dataloaders["test"], options))
+            metrics.update(get_metrics(model, dataloaders["test"], "test", options))
         
         if(metrics):
-            logging.info("Evaluation")
+            logging.info(f"Epoch {epoch} evaluation results")
             for key, value in metrics.items():
                 logging.info(f"{key}: {value:.4f}")
 
