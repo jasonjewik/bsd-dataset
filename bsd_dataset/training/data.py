960a3e4462301ed2e592d1230d7ae2acc16703a6
Shashank Goel <shashankgoel@mint1.cs.ucla.edu>
2022-06-01 10:20:09 -0700

    minto changes

--- bsd_dataset/training/data.py
+++ bsd_dataset/training/data.py
@@ -37,16 +37,22 @@ def get_dataloaders(options):
     dataloaders = {}
 
     datasets = get_datasets(options)
+    input_shape, target_shape = [1, 1, 1], [1, 1, 1]
+    
     for split in ["train", "val", "test"]:
+        if(options.no_train and split == "train"): continue
+        if(options.no_val and split == "val"): continue
+        if(options.no_test and split == "test"): continue
+
         dataset = datasets.get_split(split)
-        input_shape, target_shape = dataset[0][0].shape, dataset[0][1].shape
+        input_shape, target_shape = list(dataset[0][0].shape), list(dataset[0][1].shape)
         sampler = DistributedSampler(dataset) if(options.distributed and split == "train") else None
         dataloader = torch.utils.data.DataLoader(dataset, batch_size = options.batch_size, num_workers = options.num_workers, pin_memory = (split == "train"), sampler = sampler, shuffle = (split == "train") and (sampler is None), drop_last = (split == "train"))
         dataloader.num_samples = options.batch_size * len(dataloader) if (split == "train") else len(dataset)
         dataloader.num_batches = len(dataloader)
         dataloaders[split] = dataloader
     
-    return dataloaders, list(input_shape), list(target_shape)
+    return dataloaders, input_shape, target_shape
 
 def load(options):    
     return get_dataloaders(options)
