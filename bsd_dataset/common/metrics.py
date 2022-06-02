ae8c7f68a5f8afd553faddcc58f03f9d6f977869
Shashank Goel <shashankgoel@mint1.cs.ucla.edu>
2022-06-02 15:29:44 -0700

    bug fixes

--- bsd_dataset/common/metrics.py
+++ bsd_dataset/common/metrics.py
@@ -1,5 +1,6 @@
 import torch
 import pandas as pd
+from scipy import stats
 
 def rmse(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
     return rmse_ignore_nans(y_pred, y_true, mask)
@@ -7,8 +8,8 @@ def rmse(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
 def bias(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
     return bias_ignore_nans(y_pred, y_true, mask)
 
-def pearsons_r(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
-    return pearsons_r_ignore_nans(y_pred, y_true, mask)
+def pearsonr(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
+    return pearsonr_ignore_nans(y_pred, y_true, mask)
 
 def mask_with_zeros(t: torch.Tensor, mask: torch.BoolTensor):
     z = torch.zeros_like(t)
@@ -16,20 +17,16 @@ def mask_with_zeros(t: torch.Tensor, mask: torch.BoolTensor):
     return result
 
 def rmse_ignore_nans(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
-    mse_loss = torch.nn.MSELoss()
-    masked_y_true = mask_with_zeros(y_true, mask)
-    masked_y_pred = mask_with_zeros(y_pred, mask)
-    return torch.sqrt(mse_loss(masked_y_pred, masked_y_true))
+    masked_y_true = y_true[~mask]
+    masked_y_pred = y_pred[~mask]
+    return (masked_y_pred - masked_y_true).square().mean().sqrt().cpu().item()
 
 def bias_ignore_nans(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
-    masked_y_true = mask_with_zeros(y_true, mask)
-    masked_y_pred = mask_with_zeros(y_pred, mask)
-    return torch.sum(masked_y_pred - masked_y_true)
+    masked_y_true = y_true[~mask]
+    masked_y_pred = y_pred[~mask]
+    return (masked_y_pred - masked_y_true).abs().mean().cpu().item()
 
-def pearsons_r_ignore_nans(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
-    masked_y_true = mask_with_zeros(y_true, mask)
-    masked_y_pred = mask_with_zeros(y_pred, mask)
-    y_true_df = pd.DataFrame(masked_y_true.cpu().flatten(1))
-    y_pred_df = pd.DataFrame(masked_y_pred.cpu().flatten(1))    
-    r = y_pred_df.corrwith(y_true_df, axis=1).mean()
-    return r
+def pearsonr_ignore_nans(y_pred: torch.Tensor, y_true: torch.Tensor, mask: torch.BoolTensor):
+    masked_y_true = y_true[~mask]
+    masked_y_pred = y_pred[~mask]
+    return stats.pearsonr(masked_y_true.cpu().numpy(), masked_y_pred.cpu().numpy())[0]
