a8e283cc0c5b0b8b368ce87d27869a8b2cf9eace
Jason Jewik <jason.jewik@cs.ucla.edu>
2022-06-02 15:28:26 -0700

    From our meeting

--- bsd_dataset/datasets/check_dataset.py
+++ bsd_dataset/datasets/check_dataset.py
@@ -2,6 +2,7 @@ from typing import Any, Callable, Dict, Tuple
 
 import skimage.transform
 import torch
+import torch.nn as nn
 
 import bsd_dataset.common.metrics as metrics
 
@@ -15,15 +16,10 @@ class Interpolator:
         y: torch.Tensor,
         info: Dict[str, Any]
     ) -> Tuple[torch.Tensor, torch.BoolTensor]:
-        # Mask nans with zero
-        yy = metrics.mask_with_zeros(y, info['y_mask'])
-        # Downsample the hi-res target to the shape of the low-res input
-        yy = torch.tensor(skimage.transform.resize(yy, x.shape[1:]))
-        # Downsample the nan mask
-        mask = info['y_mask'].to(torch.int)
-        mask = skimage.transform.resize(mask, x.shape[1:])
-        mask = torch.tensor(mask).to(bool)
-        return yy, mask
+        pool1 = nn.AdaptiveAvgPool2d(y.shape)
+        pool2 = nn.AdaptiveAvgPool2d(x.shape[1:])
+        xx = pool2(pool1(x.unsqueeze(0)))
+        return xx.squeeze()
 
     def eval(
         self,
