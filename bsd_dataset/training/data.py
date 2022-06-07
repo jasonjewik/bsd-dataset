fix normalization

bsd_dataset/training/data.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.distributed import DistributedSampler
 
 class MyDataset(Dataset):
-    def __init__(self, path, split):
+    def __init__(self, path, split, normalize=None):
         super().__init__()
         path_x = os.path.join(path, split + "_x.npy")
         path_y = os.path.join(path, split + "_y.npy")
@@ -14,11 +14,14 @@ class MyDataset(Dataset):
         if(len(self.x.shape) == 3):
             self.x = self.x.unsqueeze(1)
         self.y = torch.from_numpy(np.load(path_y)).float()
-        self.normalize = torchvision.transforms.Normalize(tuple([0.5] * self.x.shape[1]), tuple([0.5] * self.x.shape[1]))
+        self.normalize = normalize
     
     def __getitem__(self, index):
         mask = torch.zeros(self.y.shape[1:]).bool()
-        return self.normalize(self.x[index]), self.y[index], {"y_mask": mask}
+        if self.normalize is not None:
+            return self.normalize(self.x[index]), self.y[index], {"y_mask": mask}
+        else:
+            return self.x[index], self.y[index], {"y_mask": mask}
 
     def __len__(self):
         return self.x.shape[0]
@@ -26,12 +29,19 @@ class MyDataset(Dataset):
 def get_dataloaders(options):
     dataloaders = {}
     
+    normalize = None
     for split in ["train", "val", "test"]:
         if(eval(f"options.no_{split}")):
             dataloaders[split] = None
             continue
 
-        dataset = MyDataset(options.data, split = split)
+        if normalize is None:
+            unnormalized_dataset = MyDataset(options.data, split = split)
+            mean = tuple([torch.mean(unnormalized_dataset.x[:, i]) for i in range(unnormalized_dataset.x.shape[1])])
+            std = tuple([torch.std(unnormalized_dataset.x[:, i]) for i in range(unnormalized_dataset.x.shape[1])])
+            normalize = torchvision.transforms.Normalize(mean, std)
+
+        dataset = MyDataset(options.data, split = split, normalize=normalize)
 
         input_shape, target_shape = list(dataset[0][0].shape), list(dataset[0][1].shape)
         sampler = DistributedSampler(dataset) if(options.distributed and split == "train") else None